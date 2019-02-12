43fcea8d6685954b586ff0dba98bb03c963d36f1
Claymonte <kaswic@hotmail.com>
2019-02-12 20:46:32 +0200

    id, enntity, generatedvalue

--- Bookstore/src/main/java/com/example/Bookstore/domain/Book.java
+++ Bookstore/src/main/java/com/example/Bookstore/domain/Book.java
@@ -1,9 +1,14 @@
 package com.example.Bookstore.domain;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Entity
 public class Book {
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
 	private String title;
 	private String author;
 	private int isbn;
