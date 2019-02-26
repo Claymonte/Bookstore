714a699396b507108f1f5bebd7f1a3036aa41efe
Claymonte <kaswic@hotmail.com>
2019-02-26 18:49:27 +0200

    fixed application

--- Bookstore/src/main/java/com/example/Bookstore/BookstoreApplication.java
+++ Bookstore/src/main/java/com/example/Bookstore/BookstoreApplication.java
@@ -7,6 +7,8 @@ import org.springframework.context.annotation.Bean;
 
 import com.example.Bookstore.domain.Book;
 import com.example.Bookstore.web.BookRepository;
+import com.example.Bookstore.web.Category;
+import com.example.Bookstore.web.CategoryRepository;
 
 @SpringBootApplication
 public class BookstoreApplication {
@@ -15,13 +17,18 @@ public class BookstoreApplication {
 	}
 
 	@Bean
-	public CommandLineRunner demo(BookRepository brepository) {
+	public CommandLineRunner demo(BookRepository brepository, CategoryRepository crepository) {
 		return (args) -> {
+			
+			crepository.save(new Category("Fantasy"));
+			crepository.save(new Category("Fiction"));
+			crepository.save(new Category("Animals"));
+			crepository.save(new Category("Programming"));
 
-			brepository.save(new Book("Hobitti", "K. Wickholm", 2018, 123456, 20.00));
-			brepository.save(new Book("Muumit", "K. Wickholm", 2011, 1234567, 25.00));
-			brepository.save(new Book("Koirankasvatus", "K. Wickholm", 2015, 12345678, 34.00));
-			brepository.save(new Book("Koodaamisen alkeet", "K. Wickholm", 1987, 1234556789, 50.00));
+			brepository.save(new Book("Hobitti", "K. Wickholm", 2018, 123456, 20.00, crepository.findByName("Fantasy").get(0)));
+			brepository.save(new Book("Muumit", "K. Wickholm", 2011, 1234567, 25.00, crepository.findByName("Fiction").get(0)));
+			brepository.save(new Book("Koirankasvatus", "K. Wickholm", 2015, 12345678, 34.00, crepository.findByName("Animals").get(0)));
+			brepository.save(new Book("Koodaamisen alkeet", "K. Wickholm", 1987, 1234556789, 50.00, crepository.findByName("Programming").get(0)));
 
 		}; 
 	}
