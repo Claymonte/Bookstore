dca99991b6a00aaf1ccbda4fa1712b6fcb0d374e
Claymonte <kaswic@hotmail.com>
2019-03-06 09:16:28 +0200

    added repository rest resource to book repository

--- Bookstore/src/main/java/com/example/Bookstore/web/BookRepository.java
+++ Bookstore/src/main/java/com/example/Bookstore/web/BookRepository.java
@@ -2,12 +2,15 @@ package com.example.Bookstore.web;
 
 import java.util.List;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import com.example.Bookstore.domain.Book;
 
 //by extending CrudReposity we can use several methods for saving, deleting, and finding book entities.
+@RepositoryRestResource
 public interface BookRepository extends CrudRepository<Book,Long> {
 	//defining query method
-	List<Book>findByTitle(String Title);
+	List<Book>findByTitle(@Param("title") String Title);
 	
 }
