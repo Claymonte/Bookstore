3abaae189ecaf238ae949c03afa85c5665f98851
Claymonte <kaswic@hotmail.com>
2019-03-13 10:59:22 +0200

    fixed stuff

--- Bookstore/src/main/java/com/example/Bookstore/web/UserDetailServiceImpl.java
+++ Bookstore/src/main/java/com/example/Bookstore/web/UserDetailServiceImpl.java
@@ -5,12 +5,12 @@ import org.springframework.security.core.authority.AuthorityUtils;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-
+import org.springframework.stereotype.Service;
 
 import com.example.Bookstore.domain.User;
 import com.example.Bookstore.domain.UserRepository;
 
-
+@Service
 public class UserDetailServiceImpl implements UserDetailsService  {
 	private final UserRepository repository;
 
