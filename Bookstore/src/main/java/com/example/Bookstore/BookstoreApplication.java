be973730429e999a5c76fca2e4b644a857a4fdfc
Claymonte <kaswic@hotmail.com>
2019-03-13 10:38:33 +0200

    added bchash encryption and few users with cmdrunner

--- Bookstore/src/main/java/com/example/Bookstore/BookstoreApplication.java
+++ Bookstore/src/main/java/com/example/Bookstore/BookstoreApplication.java
@@ -6,6 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
 import com.example.Bookstore.domain.Book;
+import com.example.Bookstore.domain.User;
+import com.example.Bookstore.domain.UserRepository;
 import com.example.Bookstore.web.BookRepository;
 import com.example.Bookstore.web.Category;
 import com.example.Bookstore.web.CategoryRepository;
@@ -17,8 +19,18 @@ public class BookstoreApplication {
 	}
 
 	@Bean
-	public CommandLineRunner demo(BookRepository brepository, CategoryRepository crepository) {
+	public CommandLineRunner demo(BookRepository brepository, CategoryRepository crepository, UserRepository urepository) {
 		return (args) -> {
+			
+			// Create users: admin/admin user/user
+			User user1 = new User("user"
+			,
+			"$2a$06$3jYRJrg0ghaaypjZ/.g4SethoeA51ph3UD4kZi9oPkeMTpjKU5uo6", "USER");
+			User user2 = new User("admin"
+			,
+			"$2a$10$0MMwY.IQqpsVc1jC8u7IJ.2rT8b0Cd3b3sfIBGV2zfgnPGtT4r0.C", "ADMIN");
+			urepository.save(user1);
+			urepository.save(user2);
 			
 			crepository.save(new Category("Fantasy"));
 			crepository.save(new Category("Fiction"));
